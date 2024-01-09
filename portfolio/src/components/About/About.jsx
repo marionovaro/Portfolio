189ee573f94932a023d4eae77a5bce1e91b43f11
marionovaro <marionovaro04@gmail.com>
2024-01-09 19:47:20 +0100

    about me

--- portfolio/src/components/About/About.jsx
+++ portfolio/src/components/About/About.jsx
@@ -8,13 +8,15 @@ export const About = () => {
           <div id="grid-1">
             <img src="https://res-console.cloudinary.com/dx2arqne6/thumbnails/v1/image/upload/v1704814279/MjAyMjEyMDZfMTI0MTM5X2QwanZrbg==/template_primary/Y19jcm9wLHdfMTgzNixoXzMxNzkuMjI=" alt="indonesia beach picture" />
           </div>
-          <h1 id="grid-2">2</h1>
-          <h1 id="grid-3">3</h1>
+          <p id="grid-2">19 años de edad, vida dedicada al deporte y la programación, e incansable <span className="sorprendentes">aventurero</span>. Así me describiria a grandes rasgos. <br /> <br /> Disfruto viviendo mi vida como un constante desafío y canalizo mi energia física a través del surf y el gimnasio, y la mental a través de mi gran pasión: <span id="programar-word">programar</span> </p>
+          <div id="grid-3">
+            <img src="https://res.cloudinary.com/dx2arqne6/image/upload/v1704814279/IMG-20221203-WA0026_ai6fny.jpg" alt="indonesia beach picture" />
+          </div>
           <h1 id="grid-4">4</h1>
           <h1 id="grid-5">5</h1>
           <h1 id="grid-6">6</h1>
           <h1 id="grid-7">7</h1>
-          <h1 id="grid-8">8</h1>
+          <h3 id="grid-8">Well...<br/> <span>this is me!</span></h3>
           <div id="grid-9">
             <img src="https://res-console.cloudinary.com/dx2arqne6/thumbnails/v1/image/upload/v1704814279/MjAyMjExMTlfMDY1NjU0X2Z4ZGdzMw==/template_primary/Y19jcm9wLHdfMzI2NCxoXzE1NzU=" alt="indonesia volcano picture" />
           </div>
