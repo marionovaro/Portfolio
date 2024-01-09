202ff702a367a284be87c9b6d148507a6c4cc1b3
marionovaro <marionovaro04@gmail.com>
2024-01-09 17:01:44 +0100

    jugando con grid

--- portfolio/src/components/About/About.jsx
+++ portfolio/src/components/About/About.jsx
@@ -5,7 +5,9 @@ export const About = () => {
       <div id="projects-section">
         <h1 id="projects-section-title">About Me</h1>
         <section id="about-section">
-          <img id="grid-1" src="https://res.cloudinary.com/dx2arqne6/image/upload/v1704814279/20221206_124139_d0jvkn.jpg"/>
+          <div id="grid-1">
+            <img src="https://res-console.cloudinary.com/dx2arqne6/thumbnails/v1/image/upload/v1704814279/MjAyMjEyMDZfMTI0MTM5X2QwanZrbg==/template_primary/Y19jcm9wLHdfMTgzNixoXzMxNzkuMjI=" alt="indonesia beach picture" />
+          </div>
           <h1 id="grid-2">2</h1>
           <h1 id="grid-3">3</h1>
           <h1 id="grid-4">4</h1>
@@ -13,7 +15,9 @@ export const About = () => {
           <h1 id="grid-6">6</h1>
           <h1 id="grid-7">7</h1>
           <h1 id="grid-8">8</h1>
-          <h1 id="grid-9">9</h1>
+          <div id="grid-9">
+            <img src="https://res-console.cloudinary.com/dx2arqne6/thumbnails/v1/image/upload/v1704814279/MjAyMjExMTlfMDY1NjU0X2Z4ZGdzMw==/template_primary/Y19jcm9wLHdfMzI2NCxoXzE1NzU=" alt="indonesia volcano picture" />
+          </div>
         </section>
       </div>
     </>
