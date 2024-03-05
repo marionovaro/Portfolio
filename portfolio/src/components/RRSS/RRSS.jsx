82ca32ea6c76c442718b2ae7c73595bab1f39544
marionovaro <marionovaro04@gmail.com>
2024-03-05 10:29:20 +0100

    cambios finales

--- portfolio/src/components/RRSS/RRSS.jsx
+++ portfolio/src/components/RRSS/RRSS.jsx
@@ -1,12 +1,15 @@
 import "./RRSS.css"
+import { useNavigate } from "react-router-dom"
+
+let navigate = useNavigate()
 
 export const RRSS = () => {
   return (
     <ul id="icons-list">
-      <li className="icons linkedin"><img src="https://cdn-icons-png.flaticon.com/128/2111/2111499.png" alt="linkedin icon"/></li>
-      <li className="icons github"><img src="https://cdn-icons-png.flaticon.com/128/733/733553.png" alt="github icon"/></li>
+      <li className="icons linkedin" onClick={navigate("https://www.linkedin.com/in/mario-novaro-056503226/")}><img src="https://cdn-icons-png.flaticon.com/128/2111/2111499.png" alt="linkedin icon"/></li>
+      <li className="icons github" onClick={navigate("https://github.com/marionovaro")}><img src="https://cdn-icons-png.flaticon.com/128/733/733553.png" alt="github icon"/></li>
       <li className="icons gmail"><img src="https://cdn-icons-png.flaticon.com/128/732/732200.png" alt="gmail icon"/></li>
-      <li className="icons insta"><img src="https://cdn-icons-png.flaticon.com/128/174/174855.png" alt="instagram icon"/></li> 
+      <li className="icons insta" onClick={navigate("https://www.instagram.com/marionovaro/")}><img src="https://cdn-icons-png.flaticon.com/128/174/174855.png" alt="instagram icon"/></li> 
     </ul>
   )
 }
